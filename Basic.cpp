ad11f9fffdcf2807c3e537f89e19415f85d5ead4
Tashin Parvez <84122972+TashinParvez@users.noreply.github.com>
2023-10-28 01:25:51 +0600

    Update Basic.cpp

--- Basic.cpp
+++ Basic.cpp
@@ -1,13 +1,29 @@
-//        ****************  Author :  Tashin.Parvez  *************************\
-//        ****************  Updated:    22-06-23     *************************\
-
-#include <bits/stdc++.h>
-#define    CRACKED   return 0;
-#define    nl        endl;              // NewLine
-using  namespace std;
-
-int32_t main()
-{   
-    
-    CRACKED;
+{"": {
+	"prefix": "basic",
+	"body": [
+	  "",
+	  "//        ****************  Author :  Tashin.Parvez  ****************",
+	  "//        ************* United International University *************",
+	  "//        ****************  Updated:    27/10/23     ****************",
+	  "",
+	  "#include <bits/stdc++.h>",
+	  "#define    CRACKED        return 0;",
+	  "#define    nl             endl            // NewLine",
+	  "#define    null           NULL",
+	  "#define    sp             \" \"             // space",
+	  "#define    setDec(x)      fixed << setprecision(x) ",
+	  "#define    len(s)         s.length()",
+	  "#define    deb(n)         cout << \"Tashin   \" << #n << \" = \" << n << endl; // debug code",
+	  "",
+	  "using  namespace std;",
+	  "",
+	  "int32_t main()",
+	  "{   ",
+	  "    ",
+	  "    CRACKED;",
+	  "}",
+	  ""
+	],
+	"description": ""
+  }
 }
